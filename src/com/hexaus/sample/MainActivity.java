3de1ec33c6b1cc9debe085bbd9d1a40191f7cd46
smunchang <smunchang@gmail.com>
2014-10-15 09:35:59 +0700

    0.1.6

--- src/com/hexaus/sample/MainActivity.java
+++ src/com/hexaus/sample/MainActivity.java
@@ -2,7 +2,7 @@ package com.hexaus.sample;
 
 import com.hexaus.sdk.Hexaus;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -114,26 +114,6 @@ public class MainActivity extends ActionBarActivity {
 	
 	
 	
-	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
 	
 }
